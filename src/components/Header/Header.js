4f17e79fe2439b2ab1812e4611ca07afd1c753eb
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2019-11-10 22:06:29 -0500

    add brain icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Header.css';
 
 
 const Header = () => {
   return (
     <div>
-      <h1 className="f1 mt0"><span><img style={{width: "80px", position: "relative", top: "10px"}} src="https://i.dlpng.com/static/png/2344739-face-facial-identity-recognise-recognition-scan-technology-icon-recognise-png-512_512_preview.png" alt=""/></span>BrainScan</h1>
+      <h1 className="f1 mt0"><i class="fas fa-brain"></i> BrainScan</h1>
     </div>
   )
 }
